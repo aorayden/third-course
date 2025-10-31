3594f114c61e2ba0b5d3dfccad0b45e63351439c
aorayden <aorayden@icloud.com>
2025-10-31 20:06:29 +0500

    Clear unused imports

--- MAD/OrderCoffee/app/src/main/java/com/example/ordercoffee/MainActivity.java
+++ MAD/OrderCoffee/app/src/main/java/com/example/ordercoffee/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.ordercoffee;
 
-import static java.lang.System.in;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -16,14 +14,7 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editName;
